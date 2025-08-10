324c7a6005f63aa686bac41d52738ad11c9ff152
Sanket <sanket.gupte542@gmail.com>
2025-08-10 22:02:04 +0530

    adding home and header components and updating the images to show on home screen

--- crud-ui/src/app/app.routes.ts
+++ crud-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Header } from './header/header';
+import { Home } from './home/home'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'header', component: Header}, //Component for header
+    {path: '', component: Home}, //Component for home
+];
--- crud-ui/src/app/app.ts
+++ crud-ui/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header], //To include the Header component in app.html
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
